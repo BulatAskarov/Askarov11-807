43b96ca5a003f579e148b1e45d014c33113c458f
BIAskrov <BIAskarov@stud.kpfu.ru>
2018-09-16 18:35:10 +0300

    hw03 is completed

--- lesson03/task4.java
+++ lesson03/task4.java
@@ -7,14 +7,22 @@ public class task4 {
         Scanner sc = new Scanner(System.in);
         System.out.println("введите колво чисел");
         int n = sc.nextInt();
+        int b = n / 2;
         System.out.println("введите массив");
         int[] arr = new int[n];
         String a = "yes";
         for (int i = 0; i < n; i++) {
             arr[i] = sc.nextInt();
         }
-        for (int i = 1; i < n; i++) {
-            if (arr[i] > arr[i - 1]) {
+        for (int i = 1; i < b; i++) {
+            if (arr[i] >= arr[i - 1]) {
+                a = "yes";
+            } else {
+                a = "no";
+            }
+        }
+        for (int i = b + 2; i < n; i++) {
+            if (arr[i] <= arr[i - 1]) {
                 a = "yes";
             } else {
                 a = "no";
