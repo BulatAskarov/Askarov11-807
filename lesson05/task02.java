1f66079e26de90754a23a5dd27522546e948f275
BIAskrov <BIAskarov@stud.kpfu.ru>
2018-09-24 21:09:54 +0300

    task2 added

---
+++ lesson05/task02.java
@@ -0,0 +1,20 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class task02 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("введите слово: ");
+        String s1 = sc.next();
+        int a = s1.length();
+        char b = 42;
+        char[] arr = s1.toCharArray();
+        for (int i = 0; i < a; i++) {
+            if (arr[i] >= '0' & arr[i] <= '9') {
+                arr[i] = '*';
+            }
+            System.out.print(arr[i]);
+        }
+    }
+}
